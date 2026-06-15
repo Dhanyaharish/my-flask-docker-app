bae505fabefd761e0758247e92467ce80aadaac7
Dhanyaharish <dhanyaharish639@gmail.com>
2026-06-15 12:17:01 +0530

    Add deployment job

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def hello():
     return "Hello from your Dockerized App!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
